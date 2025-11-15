dfcbbd83805d4e0a56b97df59aa4a2fe108c7b12
CoolSpy3 <coolspythree@gmail.com>
2025-11-14 18:21:28 -0800

    support multiple datasets in grids test

--- tests/n2_grids/TestN2.jl
+++ tests/n2_grids/TestN2.jl
@@ -11,9 +11,9 @@ using Logging
 using Statistics
 using ZipFile
 
-function read_csv(name::String, zip_reader::ZipFile.Reader)
-    matching_files = filter(f -> endswith(f.name, name), zip_reader.files)
-    @assert length(matching_files) == 1 "Expected exactly one file matching $name, found $(length(matching_files))"
+function read_csv(prefix::String, name::String, zip_reader::ZipFile.Reader)
+    matching_files = filter(f -> startswith(f.name, prefix) && endswith(f.name, name), zip_reader.files)
+    @assert length(matching_files) == 1 "Expected exactly one file matching $name with prefix $prefix, found $(length(matching_files))"
     file = matching_files[1]
     return CSV.File(file, header = false) |> CSV.Tables.matrix
 end
@@ -21,6 +21,9 @@ end
 function (@main)(args)
     warn_on_nan = false
 
+    datasets = [
+        "./tests/n2_grids/N2_grids.zip",
+    ]
     # Setup mappings between result names and csv files
     test_observables = Dict(
         "Density" => "Densities.csv",
@@ -33,8 +36,6 @@ function (@main)(args)
         @warn "NaN warnings are disabled."
     end
 
-    grids_zip = ZipFile.Reader("./tests/n2_grids/N2_grids.zip")
-
     graph = Graphs.linear_chain(4)
 
     generated_statistics = Dict{String,Vector{Any}}("N" => [], "U" => [])
@@ -42,98 +43,28 @@ function (@main)(args)
         generated_statistics[observable_name] = []
     end
 
-    for test_set in grids_zip.files
-        parsed_name = match(r"2x2_N(\d+)_U(\d+)_Tmu.zip", test_set.name)
-        if parsed_name === nothing
-            continue
-        end
+    for zip_file in datasets
+        grids_zip = ZipFile.Reader(zip_file)
 
-        N = parse(Int, parsed_name.captures[1])
-        U = parse(Float64, parsed_name.captures[2])
-
-        test_config = HubbardDiagonalization.TestConfiguration(
-            num_colors = N,
-            t = 2.0,
-            u_test = 0.0,
-            U = U
-        )
-
-        observables = HubbardDiagonalization.default_observables(test_config, graph)
-
-        @info "Running tests for N=$N, U=$U..."
-
-        # Reading Nested Zip Files: https://stackoverflow.com/a/44877369
-        buffer = IOBuffer(read(test_set))
-        test_data = ZipFile.Reader(buffer)
-
-        u_vals = read_csv("mu_vals.csv", test_data)[:, 1]
-        T_vals = read_csv("T_vals.csv", test_data)[:, 1]
-
-        # Map file names to expected data
-        expected_data = Dict{String,AbstractMatrix{Float64}}()
-
-        # Read in the test files
-        for results_file in unique(values(test_observables))
-            expected = read_csv(results_file, test_data)
-            @assert size(expected) == (length(T_vals), length(u_vals)) "Expected data size mismatch for $results_file"
-            expected_data[results_file] = expected
-        end
-
-        # Temporarily disable info logging for cleaner test output
-        disable_logging(Logging.Info)
-        results = HubbardDiagonalization.diagonalize_and_compute_observables(
-            T_vals,
-            u_vals,
-            test_config,
-            graph,
-            observables...,
-        )
-        disable_logging(Logging.Debug)
-
-        push!(generated_statistics["N"], N)
-        push!(generated_statistics["U"], U)
-        for (observable_name, results_file) in test_observables
-            expected = expected_data[results_file]
-            computed = results[observable_name]
-
-            @assert size(expected) == size(computed) "Size mismatch for observable $observable_name at N=$N, U=$U"
-
-            if warn_on_nan && any(isnan, expected)
-                @warn "Expected data contains NaN values for N=$N, U=$U, observable=$observable_name at " *
-                        "$(u_vals[findfirst(isnan, expected)]) < u < $(u_vals[findlast(isnan, expected)]). " *
-                        "These values will be ignored in the comparison."
-            end
-            if warn_on_nan && any(isnan, computed)
-                @warn "Computed data contains NaN values for N=$N, U=$U, observable=$observable_name at " *
-                        "$(u_vals[findfirst(isnan, computed)]) < u < $(u_vals[findlast(isnan, computed)]). " *
-                        "These values will be ignored in the comparison."
+        for test_set in grids_zip.files
+            parsed_name = match(r"2x2_N(\d+)_U(\d+)(_Tmu.zip|/$)", test_set.name)
+            if parsed_name === nothing
+                continue
             end
 
-            # Filter out NaN values for comparison
-            valid_indices = @. !isnan(expected) && !isnan(computed)
-            expected = expected[valid_indices]
-            computed = computed[valid_indices]
+            N = parse(Int, parsed_name.captures[1])
+            U = parse(Float64, parsed_name.captures[2])
 
-            if observable_name == "Energy" || observable_name == "Entropy"
-                computed ./= Graphs.num_sites(graph)  # Normalize per site
-            else
-                difference = expected .- computed
-            end
-
-            difference = expected .- computed
-            max_difference = maximum(abs.(difference))
-            mean_difference = mean(abs.(difference))
-            std_difference = std(abs.(difference))
-
-            push!(
-                generated_statistics[observable_name],
-                [mean_difference, std_difference, max_difference],
-            )
+            is_nested_zip = endswith(test_set.name, ".zip")
+            # Reading Nested Zip Files: https://stackoverflow.com/a/44877369
+            zip_reader = is_nested_zip ? ZipFile.Reader(IOBuffer(read(test_set))) : grids_zip
+            prefix = is_nested_zip ? "" : test_set.name * "/"
+            run_test_set(graph, generated_statistics, zip_reader, prefix, N, U, test_observables, warn_on_nan)
         end
+
+        # Stop Julia from garbage-collecting the reader (https://github.com/fhs/ZipFile.jl/issues/14#issuecomment-1135397765)
+        close(grids_zip)
     end
-
-    # Stop Julia from garbage-collecting the reader (https://github.com/fhs/ZipFile.jl/issues/14#issuecomment-1135397765)
-    close(grids_zip)
 
     @info "Writing results..."
 
@@ -170,6 +101,91 @@ function (@main)(args)
     @info summary
 
     @info "Done!"
+end
+
+function run_test_set(
+    graph::Graphs.Graph,
+    generated_statistics::Dict{String,Vector{Any}},
+    zip_reader::ZipFile.Reader,
+    prefix::String,
+    N::Int, U::Float64,
+    test_observables::Dict{String,String},
+    warn_on_nan::Bool)
+    test_config = HubbardDiagonalization.TestConfiguration(
+        num_colors = N,
+        t = 2.0,
+        u_test = 0.0,
+        U = U
+    )
+
+    observables = HubbardDiagonalization.default_observables(test_config, graph)
+
+    @info "Running tests for N=$N, U=$U..."
+
+    u_vals = read_csv(prefix, "mu_vals.csv", zip_reader)[:, 1]
+    T_vals = read_csv(prefix, "T_vals.csv", zip_reader)[:, 1]
+
+    # Map file names to expected data
+    expected_data = Dict{String,AbstractMatrix{Float64}}()
+
+    # Read in the test files
+    for results_file in unique(values(test_observables))
+        expected = read_csv(prefix, results_file, zip_reader)
+        @assert size(expected) == (length(T_vals), length(u_vals)) "Expected data size mismatch for $results_file"
+        expected_data[results_file] = expected
+    end
+
+    # Temporarily disable info logging for cleaner test output
+    disable_logging(Logging.Info)
+    results = HubbardDiagonalization.diagonalize_and_compute_observables(
+        T_vals,
+        u_vals,
+        test_config,
+        graph,
+        observables...,
+    )
+    disable_logging(Logging.Debug)
+
+    push!(generated_statistics["N"], N)
+    push!(generated_statistics["U"], U)
+    for (observable_name, results_file) in test_observables
+        expected = expected_data[results_file]
+        computed = results[observable_name]
+
+        @assert size(expected) == size(computed) "Size mismatch for observable $observable_name at N=$N, U=$U"
+
+        if warn_on_nan && any(isnan, expected)
+            @warn "Expected data contains NaN values for N=$N, U=$U, observable=$observable_name at " *
+                    "$(u_vals[findfirst(isnan, expected)]) < u < $(u_vals[findlast(isnan, expected)]). " *
+                    "These values will be ignored in the comparison."
+        end
+        if warn_on_nan && any(isnan, computed)
+            @warn "Computed data contains NaN values for N=$N, U=$U, observable=$observable_name at " *
+                    "$(u_vals[findfirst(isnan, computed)]) < u < $(u_vals[findlast(isnan, computed)]). " *
+                    "These values will be ignored in the comparison."
+        end
+
+        # Filter out NaN values for comparison
+        valid_indices = @. !isnan(expected) && !isnan(computed)
+        expected = expected[valid_indices]
+        computed = computed[valid_indices]
+
+        if observable_name == "Energy" || observable_name == "Entropy"
+            computed ./= Graphs.num_sites(graph)  # Normalize per site
+        else
+            difference = expected .- computed
+        end
+
+        difference = expected .- computed
+        max_difference = maximum(abs.(difference))
+        mean_difference = mean(abs.(difference))
+        std_difference = std(abs.(difference))
+
+        push!(
+            generated_statistics[observable_name],
+            [mean_difference, std_difference, max_difference],
+        )
+    end
 end
 
 end
